b876f90761e446b9565a04db8ea13725cadbb637
Nicolas Acquaviva <nicolaseacquaviva@gmail.com>
2021-01-16 22:55:27 -0300

    feat: show list of subtitles

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/nicolasacquaviva/subs-finder
+
+go 1.15
+
+require (
+	github.com/gocolly/colly/v2 v2.1.0
+	github.com/manifoldco/promptui v0.8.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocolly/colly/v2"
+	"github.com/manifoldco/promptui"
+	"os"
+	"strings"
+)
+
+type Subtitle struct {
+	Author      string
+	Description string
+	Downloads   string
+	Link        string
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Enter something to look for the subtitle")
+		os.Exit(1)
+	}
+
+	name := os.Args[1]
+	url := fmt.Sprintf("https://www.subdivx.com/index.php?accion=5&masdesc=&buscar=%s&oxdown=1", strings.Join(strings.Split(name, " "), "+"))
+	c := colly.NewCollector()
+
+	var subs []Subtitle
+
+	c.OnHTML("#contenedor_izq", func(e *colly.HTMLElement) {
+		var subtitle Subtitle
+
+		e.ForEach("#menu_detalle_buscador", func(_ int, e *colly.HTMLElement) {
+			subtitle.Link = e.ChildAttr(".titulo_menu_izq", "href")
+		})
+
+		e.ForEach("#buscador_detalle", func(_ int, e *colly.HTMLElement) {
+			subtitle.Author = e.ChildText(".link1")
+			subtitle.Description = e.ChildText("#buscador_detalle_sub")
+			subtitle.Downloads = strings.Split(e.ChildText("#buscador_detalle_sub_datos"), " ")[1]
+
+			subs = append(subs, subtitle)
+		})
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		templates := &promptui.SelectTemplates{
+			Label:    "{{ . }}?",
+			Active:   "- Description: {{ .Description | cyan }} (Downloads: {{ .Downloads | cyan }})",
+			Inactive: "Description: {{ .Description }} (Downloads: {{ .Downloads }})",
+			Selected: "{{ .Description | red | cyan }}",
+		}
+
+		prompt := promptui.Select{
+			Items:     subs,
+			Label:     "Subtitles:",
+			Templates: templates,
+		}
+
+		_, result, err := prompt.Run()
+
+		if err != nil {
+			fmt.Println("Error running prompt", err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println(result)
+	})
+
+	c.OnError(func(r *colly.Response, e error) {
+		fmt.Println("on error", e.Error(), r.Body)
+	})
+
+	c.Visit(url)
+}
